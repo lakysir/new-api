6aec709e4b717a7c29edc0b8ad199a865ff6c6c5
lakyxiong <laky.xiong@aimh8.com>
2026-08-19 11:55:20 +0800

    支持sd2.5的1080p计价

--- relay/channel/task/doubao/constants.go
+++ relay/channel/task/doubao/constants.go
@@ -40,6 +40,8 @@ var videoPriceTable = map[string]map[videoPriceKey]float64{
 	"doubao-seedance-2-5-260628": {
 		{hasVideo: false}: 70.0,
 		{hasVideo: true}:  42.0,
+		{is1080p: true, hasVideo: false}: 77.0,
+		{is1080p: true, hasVideo: true}: 46.0,
 	},
 }
 
